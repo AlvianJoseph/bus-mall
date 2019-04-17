304591a5df850f6f7fcb38911c09d2b4ee6a172b
Alvian Joseph <alvianjoseph@gmail.com>
2019-04-16 17:08:48 -0700

    updating app.js

--- app.js
+++ app.js
@@ -1,15 +1,130 @@
+
 'use strict';
 
-var catalogItemArray = [];
-var timesClicked = 0;
+var allCatalogItemsArray = [];
+var itemDescriptions = [];
 
-function CatalogItem(picturePath, description, timesClicked) {
+function CatalogItem(picturePath, description) {
     this.picturePath = picturePath;
     this.description = description;
-    this.timesClicked = timesClicked;
 
-    catalogItemArray.push(this);
+    this.timesClicked = 0;
+
+    allCatalogItemsArray.push(this);
+    itemDescriptions.push(description)
+
+    this.registerClick = function () {
+        this.timesClicked++;
+    }
 }
+
+function getRandomItem() {
+    var randomNumber = Math.floor(Math.random() * allCatalogItemsArray.length);
+    return randomNumber;
+}
+
+var totalClicks = 0;
+var MAX_CLICKS = 30;
+
+var previousItemArray = [-1, -2, -3];
+var currentItemArray = [];
+
+function renderCatalogItem(event) {
+    if (event) {
+        for (var i = 0; i < allCatalogItemsArray.length; i++) {
+            if (event.target.alt == allCatalogItemsArray[i].description) {
+                allCatalogItemsArray[i].registerClick();
+            }
+        }
+        totalClicks++;
+
+        if (totalClicks === MAX_CLICKS) {
+            renderChart();
+        }
+    }
+
+    var randomItem = -1;
+    var randomItem2 = -1;
+    var randomItem3 = -1;
+    var itemImage = allCatalogItemsArray[randomItem];
+    var catalogImageReference = document.getElementById('catalog-item');
+    var catalogImageReference2 = document.getElementById('catalog-item2');
+    var catalogImageReference3 = document.getElementById('catalog-item3');
+
+    while (currentItemArray.length < 1) {
+        randomItem = getRandomItem();
+        itemImage = allCatalogItemsArray[randomItem];
+        catalogImageReference.src = itemImage.picturePath;
+        catalogImageReference.alt = itemImage.description;
+        if (!previousItemArray.includes(randomItem)) {
+            currentItemArray.push(randomItem);
+            previousItemArray = currentItemArray;
+            console.log(`current item: ${currentItemArray[0]}, ${catalogImageReference.alt}`)
+        } else {
+            console.log('Same item as before')
+            continue;
+        }
+    }
+
+    while (currentItemArray.length < 2) {
+        randomItem2 = getRandomItem();
+        itemImage = allCatalogItemsArray[randomItem2];
+        catalogImageReference2.src = itemImage.picturePath;
+        catalogImageReference2.alt = itemImage.description;
+        if (!previousItemArray.includes(randomItem2)) {
+            currentItemArray.push(randomItem2);
+            previousItemArray = currentItemArray;
+            console.log(`current item: ${currentItemArray[1]}, ${catalogImageReference2.alt}`)
+        } else {
+            console.log('Same item as before')
+            continue;
+        }
+    }
+
+    while (currentItemArray.length < 3) {
+        randomItem3 = getRandomItem();
+        itemImage = allCatalogItemsArray[randomItem3];
+        catalogImageReference3.src = itemImage.picturePath;
+        catalogImageReference3.alt = itemImage.description;
+        if (!previousItemArray.includes(randomItem3)) {
+            currentItemArray.push(randomItem3);
+            previousItemArray = currentItemArray;
+            console.log(`current item: ${currentItemArray[2]}, ${catalogImageReference3.alt}`)
+        } else {
+            console.log('Same item as before')
+            continue;
+        }
+    }
+    currentItemArray.length = 0;
+}
+
+function renderChart() {
+    var canvasReference = document.getElementById("results-chart");
+    var totalVotes = [];
+    for (var i = 0; i < allCatalogItemsArray.length; i++) {
+        totalVotes.push(allCatalogItemsArray[i].timesClicked);
+    }
+    new Chart(canvasReference, {
+        type: 'bar',
+        data: {
+            labels: itemDescriptions,  // label for each individual bar
+            datasets: [{
+                label: 'Votes Per Item',
+                data: totalVotes, // an array of the number of votes per item
+                backgroundColor: ['red', 'blue', 'green', 'orange', 'pink', 'black', 'red', 'blue', 'green', 'orange', 'pink'],
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    tick: {
+                        beginAtZero: true,
+                    }
+                }]
+            }
+        }
+    });
+};
 
 new CatalogItem("img/bag.jpg", 'A bag with r2d2 design');
 new CatalogItem("img/banana.jpg", 'A banana');
@@ -19,77 +134,24 @@ new CatalogItem("img/breakfast.jpg", 'A breakfast');
 new CatalogItem("img/bubblegum.jpg", 'bubblegum');
 new CatalogItem("img/chair.jpg", 'A chair');
 new CatalogItem("img/cthulhu.jpg", 'cthulhu');
-new CatalogItem("img/dog-duck.jpg", 'A dog with duck');
-
-function getRandomItemNumber() {
-    var randomItemNumber = Math.floor(Math.random() * catalogItemArray.length);
-    return randomItemNumber;
-}
-
-function addClick() {
-    if (timesClicked <= 25) {
-        timesClicked += 1
-        console.log(timesClicked);
-        if (timesClicked > 25) {
-            alert("Thanks for participating");
-        }
-    }
-}
-
-var previousItem = -1;
-
-function renderCatalogItem() {
-
-    var randomIndex = getRandomItemNumber();
-    var currentItem = catalogItemArray[randomIndex];
-    var catalogReference = document.getElementById('catalog-item');
-    var catalogReference2 = document.getElementById('catalog-item2');
-    var catalogReference3 = document.getElementById('catalog-item3');
-
-    function firstImage() {
-        randomIndex = getRandomItemNumber();
-        currentItem = catalogItemArray[randomIndex];
-        catalogReference.src = currentItem.picturePath;
-        catalogReference.alt = currentItem.description;
-        while (randomIndex === previousItem) {
-            randomIndex = getRandomItemNumber();
-        }
-        previousItem = currentItem;
-    }
-
-    function secondImage() {
-        randomIndex = getRandomItemNumber();
-        currentItem = catalogItemArray[randomIndex];
-        catalogReference2.src = currentItem.picturePath;
-        catalogReference2.alt = currentItem.description;
-        while (randomIndex === previousItem) {
-            randomIndex = getRandomItemNumber();
-        }
-        previousItem = currentItem;
-    }
-
-    function thirdImage() {
-        randomIndex = getRandomItemNumber();
-        currentItem = catalogItemArray[randomIndex];
-        catalogReference3.src = currentItem.picturePath;
-        catalogReference3.alt = currentItem.description;
-        while (randomIndex === previousItem) {
-            randomIndex = getRandomItemNumber();
-        }
-        previousItem = currentItem;
-    }
-
-    firstImage();
-    secondImage();
-    thirdImage();
-}
+new CatalogItem("img/dragon.jpg", 'A dragon');
+new CatalogItem("img/pen.jpg", 'A pen');
+new CatalogItem("img/pet-sweep.jpg", 'a pet sweep');
+new CatalogItem("img/scissors.jpg", 'scissors');
+new CatalogItem("img/shark.jpg", 'A shark');
+new CatalogItem("img/sweep.png", 'sweep');
+new CatalogItem("img/tauntaun.jpg", 'tauntaun');
+new CatalogItem("img/unicorn.jpg", 'A unicorn');
+new CatalogItem("img/usb.gif", 'A usb');
+new CatalogItem("img/water-can.jpg", 'A water can');
+new CatalogItem("img/wine-glass.jpg", 'A glass of wine');
 
 renderCatalogItem();
-var catalogReference = document.getElementById('catalog-item');
-catalogReference.addEventListener('click', renderCatalogItem);
+var catalogImageReference = document.getElementById('catalog-item');
+catalogImageReference.addEventListener('click', renderCatalogItem);
 
-var catalogReference2 = document.getElementById('catalog-item2');
-catalogReference2.addEventListener('click', renderCatalogItem);
+var catalogImageReference2 = document.getElementById('catalog-item2');
+catalogImageReference2.addEventListener('click', renderCatalogItem);
 
-var catalogReference3 = document.getElementById('catalog-item3');
-catalogReference3.addEventListener('click', renderCatalogItem);
+var catalogImageReference3 = document.getElementById('catalog-item3');
+catalogImageReference3.addEventListener('click', renderCatalogItem);
--- test.js
+++ test.js
@@ -1,17 +1,130 @@
 
 'use strict';
 
-var AllItemsArray = [];
-var timesClicked = 0;
+var allCatalogItemsArray = [];
+var itemDescriptions = [];
 
-
-function CatalogItem(picturePath, description, timesClicked) {
+function CatalogItem(picturePath, description) {
     this.picturePath = picturePath;
     this.description = description;
-    this.timesClicked = timesClicked;
 
-    AllItemsArray.push(this);
+    this.timesClicked = 0;
+
+    allCatalogItemsArray.push(this);
+    itemDescriptions.push(description)
+
+    this.registerClick = function () {
+        this.timesClicked++;
+    }
 }
+
+function getRandomItem() {
+    var randomNumber = Math.floor(Math.random() * allCatalogItemsArray.length);
+    return randomNumber;
+}
+
+var totalClicks = 0;
+var MAX_CLICKS = 30;
+
+var previousItemArray = [-1, -2, -3];
+var currentItemArray = [];
+
+function renderCatalogItem(event) {
+    if (event) {
+        for (var i = 0; i < allCatalogItemsArray.length; i++) {
+            if (event.target.alt == allCatalogItemsArray[i].description) {
+                allCatalogItemsArray[i].registerClick();
+            }
+        }
+        totalClicks++;
+
+        if (totalClicks === MAX_CLICKS) {
+            renderChart();
+        }
+    }
+
+    var randomItem = -1;
+    var randomItem2 = -1;
+    var randomItem3 = -1;
+    var itemImage = allCatalogItemsArray[randomItem];
+    var catalogImageReference = document.getElementById('catalog-item');
+    var catalogImageReference2 = document.getElementById('catalog-item2');
+    var catalogImageReference3 = document.getElementById('catalog-item3');
+
+    while (currentItemArray.length < 1) {
+        randomItem = getRandomItem();
+        itemImage = allCatalogItemsArray[randomItem];
+        catalogImageReference.src = itemImage.picturePath;
+        catalogImageReference.alt = itemImage.description;
+        if (!previousItemArray.includes(randomItem)) {
+            currentItemArray.push(randomItem);
+            previousItemArray = currentItemArray;
+            console.log(`current item: ${currentItemArray[0]}, ${catalogImageReference.alt}`)
+        } else {
+            console.log('Same item as before')
+            continue;
+        }
+    }
+
+    while (currentItemArray.length < 2) {
+        randomItem2 = getRandomItem();
+        itemImage = allCatalogItemsArray[randomItem2];
+        catalogImageReference2.src = itemImage.picturePath;
+        catalogImageReference2.alt = itemImage.description;
+        if (!previousItemArray.includes(randomItem2)) {
+            currentItemArray.push(randomItem2);
+            previousItemArray = currentItemArray;
+            console.log(`current item: ${currentItemArray[1]}, ${catalogImageReference2.alt}`)
+        } else {
+            console.log('Same item as before')
+            continue;
+        }
+    }
+
+    while (currentItemArray.length < 3) {
+        randomItem3 = getRandomItem();
+        itemImage = allCatalogItemsArray[randomItem3];
+        catalogImageReference3.src = itemImage.picturePath;
+        catalogImageReference3.alt = itemImage.description;
+        if (!previousItemArray.includes(randomItem3)) {
+            currentItemArray.push(randomItem3);
+            previousItemArray = currentItemArray;
+            console.log(`current item: ${currentItemArray[2]}, ${catalogImageReference3.alt}`)
+        } else {
+            console.log('Same item as before')
+            continue;
+        }
+    }
+    currentItemArray.length = 0;
+}
+
+function renderChart() {
+    var canvasReference = document.getElementById("results-chart");
+    var totalVotes = [];
+    for (var i = 0; i < allCatalogItemsArray.length; i++) {
+        totalVotes.push(allCatalogItemsArray[i].timesClicked);
+    }
+    new Chart(canvasReference, {
+        type: 'bar',
+        data: {
+            labels: itemDescriptions,  // label for each individual bar
+            datasets: [{
+                label: 'Votes Per Item',
+                data: totalVotes, // an array of the number of votes per item
+                backgroundColor: ['red', 'blue', 'green', 'orange', 'pink', 'black', 'red', 'blue', 'green', 'orange', 'pink'],
+            }],
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    tick: {
+                        beginAtZero: true,
+                    }
+                }]
+            }
+        }
+    });
+};
 
 new CatalogItem("img/bag.jpg", 'A bag with r2d2 design');
 new CatalogItem("img/banana.jpg", 'A banana');
@@ -32,88 +145,6 @@ new CatalogItem("img/unicorn.jpg", 'A unicorn');
 new CatalogItem("img/usb.gif", 'A usb');
 new CatalogItem("img/water-can.jpg", 'A water can');
 new CatalogItem("img/wine-glass.jpg", 'A glass of wine');
-
-
-function getRandomItem() {
-    var randomItemNumber = Math.floor(Math.random() * AllItemsArray.length);
-    return randomItemNumber;
-}
-
-function addClick() {
-    if (timesClicked <= 25) {
-        timesClicked += 1
-        console.log(timesClicked);
-        if (timesClicked > 25) {
-            alert("Thanks for participating");
-        }
-    }
-}
-
-var previousItem = -1;
-var randomItemArray = [];
-
-function renderCatalogItem() {
-    var randomItem = -1;
-    var randomItem2 = -1;
-    var randomItem3 = -1;
-    var itemImage = AllItemsArray[randomItem];
-    var catalogImageReference = document.getElementById('catalog-item');
-    var catalogImageReference2 = document.getElementById('catalog-item2');
-    var catalogImageReference3 = document.getElementById('catalog-item3');
-
-
-    for (var i = 0; i < 1; i++) {
-        while (randomItem === previousItem) {
-            randomItem = getRandomItem();
-            itemImage = AllItemsArray[randomItem];
-            catalogImageReference.src = itemImage.picturePath;
-            catalogImageReference.alt = itemImage.description;
-            if (randomItem !== previousItem) {
-                randomItemArray.push(randomItem);
-                console.log(`current item: ${randomItem}`)
-            } else {
-                console.log('Same item as before')
-                continue;
-            }
-        }
-    }
-
-    previousItem = randomItem2;
-
-    for (var i = 0; i < 1; i++) {
-        while (randomItem2 === previousItem) {
-            randomItem2 = getRandomItem();
-            itemImage = AllItemsArray[randomItem2];
-            catalogImageReference2.src = itemImage.picturePath;
-            catalogImageReference2.alt = itemImage.description;
-            if (randomItem2 !== previousItem) {
-                randomItemArray.push(randomItem2);
-                console.log(`current item: ${randomItem2}`)
-            } else {
-                console.log('Same item as before')
-                continue;
-            }
-        }
-    }
-    previousItem = randomItem3;
-
-    for (var i = 0; i < 1; i++) {
-        while (randomItem3 === previousItem) {
-            randomItem3 = getRandomItem();
-            itemImage = AllItemsArray[randomItem3];
-            catalogImageReference3.src = itemImage.picturePath;
-            catalogImageReference3.alt = itemImage.description;
-            if (randomItem3 !== previousItem) {
-                randomItemArray.push(randomItem3);
-                console.log(`current item: ${randomItem3}`)
-            } else {
-                console.log('Same item as before')
-                continue;
-            }
-        }
-    }
-    randomItemArray.length = 0;
-}
 
 renderCatalogItem();
 var catalogImageReference = document.getElementById('catalog-item');
